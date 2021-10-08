79cac501a61d651430078d8000c3105fc4c0cb1d
jerrysdevs <siranjevee1995@gmail.com>
2021-10-08 11:52:18 +0530

    added port in env

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 import { Db, MongoClient } from "mongodb";
 const app = express();
 dotenv.config();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 // const users = [
